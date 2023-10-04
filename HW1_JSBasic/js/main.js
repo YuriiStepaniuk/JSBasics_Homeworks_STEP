dd7a1b2ba6ca6a50f2b02f050233db1ae006b36e
Yurii Stepaniuk <stepanik.ura2001@gmail.com>
2023-10-04 12:56:35 +0300

    first commit

---
+++ HW1_JSBasic/js/main.js
@@ -0,0 +1,39 @@
+////Task 1
+
+// const name = prompt('Введіть ваше ім`я: ')
+// alert(`Привіт, ${name}`)
+//console.log(`Привіт, ${name}`);
+
+////Task 2
+
+// const curYear = 2023
+// const birthYear = +prompt('Введіть вашу дату народження: ')
+// alert(`Ваш вік: ${curYear - birthYear}`)
+//console.log(`Ваш вік: ${curYear - birthYear}`);
+
+//// Task 3
+
+// const a = +prompt('Введіть сторону квадрату: ')
+// alert(`Периметр квадрату = ${a * 4} (a * 4)`)
+// console.log(`Периметр квадрату = ${a * 4} (a * 4)`);
+
+//// Task 4
+
+// const PI = 3.14159
+// const r = +prompt('Введіть радіус кола: ')
+// alert(`Площа кола = ${PI * (r ** 2)}`)
+// console.log(`Площа кола = ${PI * (r ** 2)}`);
+
+//// Task 5
+
+// const distance = +prompt('Введіть к-сть кілометрів між містами: ')
+// const hours = +prompt('Введіть к-сть годин, за яку ви хочете подолати відстань: ')
+// alert(`Вам знадобиться швидкість ${(distance / hours).toFixed(2)} км/год`)
+// console.log(`Вам знадобиться швидкість ${(distance / hours).toFixed(2)} км/год`);
+
+//// Task 6
+
+// const exchange = 0.95
+// const dollars = +prompt('Введіть к-сть доларів для конвертації: ')
+// alert(`За поточним курсом USD / EUR (${exchange}) ви отримаєте: ${dollars * exchange}`)
+// console.log(`За поточним курсом USD / EUR (${exchange}) ви отримаєте: ${dollars * exchange}`);
